c2aea38abba6df8930531ae81b1d2fd30b8f1c20
RamosFe <fedearamos97@gmail.com>
2022-05-13 18:50:24 -0300

    Deleted firebase from backend.
    Firebase storage must be handle by frontend.

--- app/controllers/songControllers.js
+++ app/controllers/songControllers.js
@@ -1,12 +1,5 @@
-const { Readable } = require("stream");
 const ApiError = require("../errors/apiError");
 const Song = require("../models/songModel");
-
-const firebaseStorage = require("../services/firebase").storage;
-const firebaseSongsManager =
-  require("../utils/firebaseSongsManager").FirebaseSongsManager;
-
-const songsManager = new firebaseSongsManager(firebaseStorage);
 
 const getAllSongsByQuery = async (req, res, next) => {
   const albumId = req.query.album;
@@ -56,49 +49,24 @@ const getSongById = async (req, res, next) => {
 };
 
 const createSong = async (req, res, next) => {
-  const { title, artistId, albumId, duration } = req.body;
+  const { title, artistId, albumId, duration, url } = req.body;
+  try {
+    const newSong = new Song({
+      title: title,
+      artistId: artistId,
+      albumId: albumId,
+      duration: duration,
+      url: url,
+    });
 
-  if (!req.file) {
-    next(ApiError.invalidArguments("No file uploaded !"));
+    await newSong.save();
+
+    res.status(201).send({});
+  } catch (err) {
+    console.log(err);
+    next(ApiError.invalidArguments(`Invalid arguments passed`));
     return;
   }
-
-  // Create Song uploader for loading the song to firebase Storage
-  const songUploader = songsManager.createSongUploader(
-    req.file,
-    title,
-    albumId,
-    artistId
-  );
-
-  // Set Up all the events
-  songUploader.setUpErrorEvent((err) => next(ApiError.internalError(err)));
-  songUploader.setUpFinishEvent(async () => {
-    try {
-      const newSong = new Song({
-        title: title,
-        artistId: artistId,
-        albumId: albumId,
-        duration: duration,
-        url: songUploader.getFilePublicUrl(),
-      });
-
-      const mongoCreatedSong = await newSong.save();
-
-      res.status(201).send({
-        id: mongoCreatedSong.id,
-        fileName: songUploader.getFileName(),
-        fileLocation: songUploader.getFilePublicUrl(),
-      });
-    } catch (err) {
-      console.log(err);
-      next(ApiError.invalidArguments(`Invalid arguments passed`));
-      return;
-    }
-  });
-
-  // Set Up the input stream
-  songUploader.setUpInputPipe(Readable.from(req.file.buffer.toString()));
 };
 
 module.exports = {
