25f2665e127d6e426ce2557fbd937fcf2aec0cc6
RamosFe <fedearamos97@gmail.com>
2022-05-23 20:57:08 -0300

    Added protected urls for songs and albums.

--- app/routes/albumRoutes.js
+++ app/routes/albumRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const albumControllers = require("../controllers/albumController");
+const {protectUrlByAlbumOwner} = require("../middleware/authorizationMiddleware");
 
 router.get("/", albumControllers.getAllAlbumsByQuery);
 router.get("/:id", albumControllers.getAlbumById);
-router.post("/", albumControllers.createAlbum);
-router.post("/:id/add-song", albumControllers.createSongAndAddToAlbum);
+router.post("/", protectUrlByAlbumOwner, albumControllers.createAlbum);
+router.post("/:id/add-song", protectUrlByAlbumOwner, albumControllers.createSongAndAddToAlbum);
 
 module.exports = router;
--- app/routes/songRoutes.js
+++ app/routes/songRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const songControllers = require("../controllers/songControllers");
+const {protectUrlBySongOwner} = require("../middleware/authorizationMiddleware");
 
 router.get("/", songControllers.getAllSongsByQuery);
 router.get("/:id", songControllers.getSongById);
-router.post("/", songControllers.createSong);
+router.post("/", protectUrlBySongOwner, songControllers.createSong);
 
 module.exports = router;
