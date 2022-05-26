d2c0c6c435ea547bc78a31e390741e49b0044877
NachitoPerez <irperez@fi.uba.ar>
2022-05-26 12:51:54 -0300

    fix: get songs by title added in getAllSongsByQuery

--- app/routes/songRoutes.js
+++ app/routes/songRoutes.js
@@ -5,7 +5,6 @@ const songControllers = require("../controllers/songControllers");
 
 router.get("/", songControllers.getAllSongsByQuery);
 router.get("/:id", songControllers.getSongById);
-router.get("/:title", songControllers.getSongsByTitle);
 router.post("/", songControllers.createSong);
 
 module.exports = router;
