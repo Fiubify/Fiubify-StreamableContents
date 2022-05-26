b89ccb3f823b9df9d1b104b8cb0ec0c6efbcc3d6
pablo.berrotaran <pberrotaran@keepcon.com>
2022-05-26 15:44:59 -0300

    Adds track to playlist

--- app/routes/playlistRoutes.js
+++ app/routes/playlistRoutes.js
@@ -6,5 +6,6 @@ const playlistController = require("../controllers/playlistController")
 router.get("/", playlistController.getPlaylists)
 router.get("/:id", playlistController.getPlaylist)
 router.post("/", playlistController.createPlaylist)
+router.post("/:id/add-track", playlistController.addTrackToPlaylist);
 
 module.exports = router
