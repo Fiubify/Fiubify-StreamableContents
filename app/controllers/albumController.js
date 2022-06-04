2015a4f2e579c5112cdcc6daeba06c57d65487c1
RamosFe <fedearamos97@gmail.com>
2022-06-04 18:11:54 -0300

    Added console log for validating error.

--- app/controllers/albumController.js
+++ app/controllers/albumController.js
@@ -64,6 +64,7 @@ const createAlbum = async (req, res, next) => {
 
         res.status(201).send({})
     } catch (err) {
+        console.log(err);
         next(ApiError.invalidArguments("Invalid arguments passed"));
         return
     }
