ae84efe32c1b6511893520baeddcdcd65883836e
NachitoPerez <irperez@fi.uba.ar>
2022-06-12 00:41:22 -0300

    fix: fix async tracks populate

--- app/controllers/playlistController.js
+++ app/controllers/playlistController.js
@@ -43,9 +43,8 @@ const getPlaylists = async (req, res, next) => {
 
 const getPlaylist = async (req, res, next) => {
     const playlistId = req.params.id
-    const requestedPlaylist = await Playlist.findById(playlistId).then((playlist) => {
-        playlist.populate('tracks');
-    })
+    const requestedPlaylist = await Playlist.findById(playlistId);
+    await requestedPlaylist.populate('tracks');
     if (requestedPlaylist == null) {
         next(ApiError.resourceNotFound(`Playlist with id ${playlistId} doesn't exist`))
         return
