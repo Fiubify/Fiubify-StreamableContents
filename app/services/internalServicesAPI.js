781b749579e889c1fbfb25d095189117a297d009
RamosFe <fedearamos97@gmail.com>
2022-06-18 18:48:06 -0300

    Changed hardcoded paths for auth.

---
+++ app/services/internalServicesAPI.js
@@ -0,0 +1,6 @@
+const internalServicesUrls = {
+    uidAuthValidationUrl: "https://fiubify-users-staging.herokuapp.com/validate/uid",
+    userAuthValidationUrl: "https://fiubify-users-staging.herokuapp.com/validate/user"
+}
+
+module.exports = internalServicesUrls
