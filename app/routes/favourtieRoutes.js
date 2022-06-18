21eb05133ca5f4e5dabffaab8efbb6627b19dd19
RamosFe <fedearamos97@gmail.com>
2022-06-17 22:13:55 -0300

    Added protected url for favourite route.
    The protected url needs uid and token for validation.

--- app/routes/favourtieRoutes.js
+++ app/routes/favourtieRoutes.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const favouritesController = require("../controllers/favouritesController");
+const {protectUrlByUid} = require("../middleware/authorizationMiddleware");
 
+if (process.env.NODE_ENV === "DEV") {
+    router.get("/:uid", favouritesController.getAllFavouritesSongs)
+    router.post("/:uid/add-song", favouritesController.addSongToFavourites);
+    router.delete("/:uid/remove-song", favouritesController.removeSongFromFavourites);
+} else {
+    router.get("/:uid", protectUrlByUid, favouritesController.getAllFavouritesSongs)
+    router.post("/:uid/add-song", protectUrlByUid, favouritesController.addSongToFavourites);
+    router.delete("/:uid/remove-song", protectUrlByUid, favouritesController.removeSongFromFavourites);
+}
 
-router.get("/:uid", favouritesController.getAllFavouritesSongs)
-router.post("/:uid/add-song", favouritesController.addSongToFavourites);
-router.delete("/:uid/remove-song", favouritesController.removeSongFromFavourites);
 
 module.exports = router;
