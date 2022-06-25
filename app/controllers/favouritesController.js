74768f8d89822f4218d19e2d70fd4d08ed5f5db0
NachitoPerez <irperez@fi.uba.ar>
2022-06-25 16:59:21 -0300

    feat: add track to favourites only if isn't already added

--- app/controllers/favouritesController.js
+++ app/controllers/favouritesController.js
@@ -1,29 +1,32 @@
 const Favourites = require("../models/favouritesModel");
 const ApiError = require("../errors/apiError")
 
+async function fetchFavourites(uid) {
+    const userFavourites = await Favourites.findOne({"uid": uid});
+    if (userFavourites == null) {
+        const newFav = new Favourites({
+            uid: uid,
+        });
+        await newFav.save();
+        return [];
+    } else {
+        userFavourites.populate('tracks');
+        return userFavourites.tracks;
+    }
+}
+
 const getAllFavouritesSongs = async (req, res, next) => {
     const userUid = req.params.uid;
 
     try {
-        const userFavourites = await Favourites.findOne({"uid": userUid});
-        if (userFavourites == null) {
-            const newFav = new Favourites({
-                uid: userUid,
-            });
-            await newFav.save();
-            res.status(200).json({data: []})
-        } else {
-            userFavourites.populate('tracks')
-            res.status(200).json({data: userFavourites.tracks});
-        }
+        const tracks = await fetchFavourites(userUid);
+        res.status(200).json({data: tracks});
     } catch (error) {
         console.log(error);
         next(ApiError.internalError(`Internal error when getting favourites for user ${userUid}`));
-        return;
     }
 
 }
-
 
 const addSongToFavourites = async (req, res, next) => {
     if (res.missingFieldsInBody.length > 0) {
@@ -35,7 +38,10 @@ const addSongToFavourites = async (req, res, next) => {
     const {songId} = req.body;
 
     try {
-        await Favourites.findOneAndUpdate({"uid": userUid}, {$push: {"tracks": songId}}, {"upsert": true});
+        const tracks = await fetchFavourites(userUid);
+        if (!(await tracks).includes(songId)) {
+            await Favourites.findOneAndUpdate({"uid": userUid}, {$push: {"tracks": songId}}, {"upsert": true});
+        }
         res.status(201).json({});
     } catch (error) {
         console.log(error);
@@ -50,7 +56,7 @@ const removeSongFromFavourites = async (req, res, next) => {
         next(ApiError.missingFieldsInBody(res.missingFieldsInBody));
         return;
     }
-    
+
     const userUid = req.params.uid;
     const {songId} = req.body;
 
